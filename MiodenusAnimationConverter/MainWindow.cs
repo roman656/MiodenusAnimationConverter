b3ed9706b11b80d2d7fe2f3f190447cb9905b3be
roman656 <romanstrah@mail.ru>
2021-07-03 00:03:53 +0300

    Removed redundant method.

--- MiodenusAnimationConverter/MainWindow.cs
+++ MiodenusAnimationConverter/MainWindow.cs
@@ -252,26 +252,7 @@ namespace MiodenusAnimationConverter
             var tempScreenshot = new Screenshot((ushort)Size.X, (ushort)Size.Y);
             tempScreenshot.SaveToPng($"screenshot_{_screenshotId}");
         }
-        
-        private static void SaveScreenshot(string filename, int windowWidth, int windowHeight)
-        {
-            var pixels = new byte[windowWidth * windowHeight * 3];
 
-            GL.PixelStore(PixelStoreParameter.PackAlignment, 1);
-            GL.ReadBuffer(ReadBufferMode.Front);
-            GL.ReadPixels(0, 0, windowWidth, windowHeight, PixelFormat.Bgr, PixelType.UnsignedByte, pixels);
-
-            var width = BitConverter.GetBytes((short) windowWidth);
-            var height = BitConverter.GetBytes((short) windowHeight);
-            byte[] header = {0,  0,  2,  0, 0, 0, 0, 0,  0, 0,  0, 0,  width[0], width[1], height[0], height[1], 24, 0b00001000};
-
-            using (var fs = File.OpenWrite(filename))
-            {
-                fs.Write(header, 0, header.Length);
-                fs.Write(pixels, 0, pixels.Length);
-            }
-        }
-        
         protected override void OnClosed()
         {
             GL.DeleteVertexArray(_vertexArray);
