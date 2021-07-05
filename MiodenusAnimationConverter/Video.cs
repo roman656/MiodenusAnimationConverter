add55281df64582288ad44e2b1b2d32134eccd7f
PoorMercymain <67076111+PoorMercymain@users.noreply.github.com>
2021-07-05 20:01:40 +0300

    Add files via upload

--- MiodenusAnimationConverter/Video.cs
+++ MiodenusAnimationConverter/Video.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace MiodenusAnimationConverter
@@ -5,29 +7,32 @@ namespace MiodenusAnimationConverter
     public class Video
     {
         public readonly string Type;
-        public readonly int Length;
-        public readonly int Bitrate;
+        public readonly uint Time;
+        public readonly uint Bitrate;
         public readonly string Filename;
-        public readonly int FPS;
+        public readonly byte Fps;
 
-        public Video(string videoType, int videoLength, int videoBitrate, string nameOfVideoFile)
+        public Video(string videoType, uint videoTime, uint videoBitrate, string nameOfVideoFile, byte videoFps)
         {
-            this.Type = videoType;
-            this.Length = videoLength;
-            this.Bitrate = videoBitrate;
-            this.Filename = nameOfVideoFile + "." + Type;
-            this.FPS = 60;
-        }
-
-        public void CreateVideo()
-        {
+            if (videoFps > 0)
+            {
+                Fps = videoFps;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid Fps value was used in constructor of video");
+            }
             
+            Type = videoType;
+            Time = videoTime;
+            Bitrate = videoBitrate;
+            Filename = nameOfVideoFile + "." + Type;
         }
 
         public override string ToString()
         {
             return
-                $"Type = {this.Type}, length = {this.Length}, bitrate = {this.Bitrate}, filename = {this.Filename}\n";
+                string.Format(CultureInfo.InvariantCulture, "Video: \n{{\n    Type: {0}\n    Time: {1}\n    Bitrate: {2}\n    Filename: {3}\n    FPS: {4}\n}}", Type, Time, Bitrate, Filename, Fps);
         }
     }
 }
