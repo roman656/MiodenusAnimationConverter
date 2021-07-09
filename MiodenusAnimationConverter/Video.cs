2b888dbb7a18a96581a09ee9237c8122f0f4476f
PoorMercymain <67076111+PoorMercymain@users.noreply.github.com>
2021-07-09 16:06:30 +0300

    Video class was slightly upgraded

--- MiodenusAnimationConverter/Video.cs
+++ MiodenusAnimationConverter/Video.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Globalization;
-using System.Drawing; //может, работает
+using System.Drawing;
+using System.IO; //может, работает
 using System.Runtime.CompilerServices;
 using AForge.Video.FFMPEG; //не работает
 
@@ -13,14 +14,9 @@ namespace MiodenusAnimationConverter
         public readonly uint Bitrate;
         public readonly string Filename;
         public readonly byte Fps;
-
         public Video(string videoType, uint videoTime, uint videoBitrate, string nameOfVideoFile, byte videoFps)
         {
-            if (videoFps <= 0)
-            {
-                throw new ArgumentException("Invalid Fps value was used in constructor of video");
-            }
-            
+            CheckArguments(videoTime, videoBitrate, nameOfVideoFile, videoFps);
             Type = videoType;
             Time = videoTime;
             Bitrate = videoBitrate;
@@ -28,25 +24,39 @@ namespace MiodenusAnimationConverter
             Fps = videoFps;
         }
 
-        public void CreateVideo(int imagesAmount/*, string path*/) //не работает
+        public void CreateVideo(int imagesAmount, string path) //не работает
         {
             System.Console.Out.WriteLine("Video creation started");
-            
-            string path = "C:\\Users\\PoorMercymain\\RiderProjects\\Miodenus1\\MiodenusAnimationConverter\\Photo\\";
-            
+
             using (var videoWriter = new VideoFileWriter())
             {
-                videoWriter.Open($"{path}video.avi", 600, 600, 15, VideoCodec.MPEG4, 10000);
+                var img = Bitmap.FromFile($"{path}screenshot_0.png");
+                
+                var width = img.Width;
+                var height = img.Height;
+                
+                videoWriter.Open($"{path}{Filename}", width, height, Fps, VideoCodec.MPEG4, (int)Bitrate);
 
                 for (int imageFrame = 0; imageFrame < imagesAmount; imageFrame++)
                 {
-                    var imgPath = string.Format("{0}screenshot_{1}.png", path, imageFrame);
-                    using (Bitmap image = Bitmap.FromFile(imgPath) as Bitmap)
+                    var file = string.Format("{0}screenshot_{1}.png", path, imageFrame);
+                    
+                    if (System.IO.File.Exists(file))
                     {
-                        videoWriter.WriteVideoFrame(image);
+                        using (Bitmap image = Bitmap.FromFile(file) as Bitmap)
+                        {
+                            videoWriter.WriteVideoFrame(image);
+                        }
                     }
+                    else
+                    {
+                        throw new FileNotFoundException($"File {file} was not found.");
+                    }
+                    
                 }
                 videoWriter.Close();
+                
+                System.Console.Out.WriteLine("Video creation finished");
             }
         }
 
@@ -55,6 +65,29 @@ namespace MiodenusAnimationConverter
             return string.Format(CultureInfo.InvariantCulture, 
                     "Video: \n{{\n    Type: {0}\n    Time: {1}\n    Bitrate: {2}\n    Filename: {3}\n    FPS: {4}\n}}", 
                     Type, Time, Bitrate, Filename, Fps);
+        }
+
+        private void CheckArguments(uint videoTimeArgument, uint videoBitrateArgument, string nameOfVideoFileArgument, byte videoFpsArgument)
+        {
+            if (videoFpsArgument <= 0)
+            {
+                throw new ArgumentException("Invalid Fps value was used in constructor of video");
+            }
+
+            if (videoBitrateArgument <= 0)
+            {
+                throw new ArgumentException("Invalid bitrate value was used in constructor of video");
+            }
+            
+            if (videoTimeArgument <= 0)
+            {
+                throw new ArgumentException("Invalid time value was used in constructor of video");
+            }
+            
+            if (nameOfVideoFileArgument == "")
+            {
+                throw new ArgumentException("Epty name of video file was used in constructor of video");
+            }
         }
     }
 }
