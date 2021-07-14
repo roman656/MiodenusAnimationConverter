d1b6ad2980520528291713063d60f5a50398d733
roman656 <romanstrah@mail.ru>
2021-07-14 18:55:57 +0300

    Added animation class (not implemented).

---
+++ MiodenusAnimationConverter/Loaders/AnimationLoader.cs
@@ -0,0 +1,10 @@
+namespace MiodenusAnimationConverter.Loaders
+{
+    public class AnimationLoader : IAnimationLoader
+    {
+        public void Load(in string filename)
+        {
+            
+        }
+    }
+}
