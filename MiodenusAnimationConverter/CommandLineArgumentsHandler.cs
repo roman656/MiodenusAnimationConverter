9d9c80985fc82df4586469d84b1e3c307343dbe7
PoorMercymain <pinegin17@bk.ru>
2021-07-15 21:50:07 +0300

    just commit

--- MiodenusAnimationConverter/CommandLineArgumentsHandler.cs
+++ MiodenusAnimationConverter/CommandLineArgumentsHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using MiodenusAnimationConverter.Exceptions;
+using OpenTK.Graphics.ES30;
 using OpenTK.Graphics.OpenGL;
 
 namespace MiodenusAnimationConverter
@@ -15,18 +16,35 @@ namespace MiodenusAnimationConverter
             " (or paths) to input file (or files), the second should be a path to the output video destination," +
             " order of other arguments is not important.";
         
-        public string[] InputKeys = {"-fps",
-                                     "-bitrate",
-                                     "-extension"
+        public string[] InputKeys = {"--fps",
+                                     "-f",
+                                     "--bitrate",
+                                     "-b",
+                                     "--extension",
+                                     "-e",
+                                     "--help",
+                                     "?",
+                                     "-h",
+                                     "--animation",
+                                     "-a",
+                                     "--models",
+                                     "-m",
+                                     "--output",
+                                     "-o"
         };
 
-        public bool[] InputKeysCheck = {false,
-                                        false,
-                                        false
+        public Dictionary<string, bool> InputKeysCheck = new Dictionary<string, bool>(){
+            ["fps"] = false,
+            ["bitrate"] = false,
+            ["extension"] = false,
+            ["animation"] = false,
+            ["models"] = false,
+            ["output"] = false
         };
 
-        public byte MinimumAmountOfArguments = 2;
-        public List<string> AnimationFile = new List<string>();
+        public byte MinimumAmountOfArguments = 3;
+        public string AnimationFile = "";
+        public List<string> ModelFiles = new List<string>();
         public string VideoFile = "";
         public string Extension = "avi";
         public int Bitrate = 10000;
@@ -34,13 +52,13 @@ namespace MiodenusAnimationConverter
 
         public MainSettings InitializeSettings()
         {
-            MainSettings settings = new MainSettings(VideoFile, Extension, Bitrate, Fps, AnimationFile);
+            MainSettings settings = new MainSettings(VideoFile, Extension, Bitrate, Fps, ModelFiles);
 
             Console.WriteLine($"settings:\n  VideoFile = {settings.VideoFile}\n  Extension = {settings.Extension}\n  Bitrate = {settings.Bitrate}\n  Fps = {settings.Fps}");
 
             for (int i = 0; i < settings.AnimationFile.Count; i++)
             {
-                Console.WriteLine($"  AnimationFile[{i}] = {settings.AnimationFile[i]}");
+                Console.WriteLine($"  ModelFiles[{i}] = {settings.AnimationFile[i]}");
             }
             
             Console.WriteLine("end settings");
@@ -52,39 +70,43 @@ namespace MiodenusAnimationConverter
         {
             var argumentsList = new List<string>(arguments);
             
-            if (argumentsList.Count == 1 && argumentsList[0] == "-i")
-            {
-                System.Console.Out.WriteLine(info);
-                Environment.Exit(0);
-            }
+            //if (argumentsList.Count == 1 && argumentsList[0] == "-i")
+            //{
+                //System.Console.Out.WriteLine(info);
+                //Environment.Exit(0);
+            //}
             
             if (argumentsList.Count < MinimumAmountOfArguments)
             {
-                throw new WrongCommandLineArgumentsException("At least 2 command line arguments should be used");
+                throw new WrongCommandLineArgumentsException($"At least {MinimumAmountOfArguments} command line arguments should be used");
             }
             else
             {
-                System.Console.Out.WriteLine(argumentsList[0]);
+                //System.Console.Out.WriteLine(argumentsList[0]);
                 
                 for ( ; argumentsList.Count!=0; )
                 {
-                    if (argumentsList[0] != InputKeys[0] && argumentsList[0] != InputKeys[1] && argumentsList[0] != InputKeys[2])
-                    {
-                            
-                        AnimationFile.Add(argumentsList[0]);
-                        argumentsList.Remove(argumentsList[0]);
-                    }
-                    else
-                    {
-                            if (argumentsList.Count < 2)
+                        
+                        //ModelFiles.Add(argumentsList[0]);
+                        //argumentsList.Remove(argumentsList[0]);
+                        foreach (string argument in argumentsList)
+                        {
+                            if (argument == InputKeys[6] || argument == InputKeys[7] || argument == InputKeys[8])
                             {
-                                throw new WrongCommandLineArgumentsException($"Argument {argumentsList[0]} should not be empty"); 
+                                Console.WriteLine(info);
+                                Environment.Exit(0);
                             }
-                            else
-                            {
-                                if (argumentsList[0] == "-fps")
+                        }
+
+                        if (argumentsList.Count < 2)
+                        {
+                            throw new WrongCommandLineArgumentsException($"Argument {argumentsList[0]} should not be empty"); 
+                        }
+                        else
+                        {
+                                if (argumentsList[0] == InputKeys[0]||argumentsList[0]==InputKeys[1])
                                 {
-                                    if (!InputKeysCheck[0])
+                                    if (!InputKeysCheck["fps"])
                                     {
                                         if (!Int32.TryParse(argumentsList[1], out Fps))
                                         {
@@ -96,7 +118,7 @@ namespace MiodenusAnimationConverter
                                             throw new WrongCommandLineArgumentsException("Fps should be greater or equal to 1 and less than 256");
                                         }
                                         
-                                        InputKeysCheck[0] = true;
+                                        InputKeysCheck["fps"] = true;
                                                                             
                                         argumentsList.Remove(argumentsList[1]);
                                         argumentsList.Remove(argumentsList[0]);
@@ -110,9 +132,9 @@ namespace MiodenusAnimationConverter
                                     
                                     
                                 }
-                                else if (argumentsList[0] == "-bitrate")
+                                else if (argumentsList[0] == InputKeys[2]||argumentsList[1]==InputKeys[3])
                                 {
-                                    if (!InputKeysCheck[1])
+                                    if (!InputKeysCheck["bitrate"])
                                     {
                                         if(!Int32.TryParse(argumentsList[1], out Bitrate))
                                         {
@@ -124,7 +146,7 @@ namespace MiodenusAnimationConverter
                                             throw new WrongCommandLineArgumentsException("Bitrate should be greater or equal to 1");
                                         }
                                         
-                                        InputKeysCheck[1] = true;
+                                        InputKeysCheck["bitrate"] = true;
                                         
                                         argumentsList.Remove(argumentsList[1]);
                                         argumentsList.Remove(argumentsList[0]);
@@ -137,13 +159,13 @@ namespace MiodenusAnimationConverter
                                     }
                                     
                                 }
-                                else if(argumentsList[0] == "-extension")
+                                else if(argumentsList[0] == InputKeys[4]||argumentsList[0]==InputKeys[5])
                                 {
-                                    if (!InputKeysCheck[2])
+                                    if (!InputKeysCheck["extension"])
                                     {
                                         Extension = argumentsList[1];
                                         
-                                        InputKeysCheck[2] = true;
+                                        InputKeysCheck["extension"] = true;
                                         
                                         argumentsList.Remove(argumentsList[1]);
                                         argumentsList.Remove(argumentsList[0]);
@@ -156,24 +178,102 @@ namespace MiodenusAnimationConverter
                                     }
                                     
                                 }
-                            }
-                    }
+                                else if (argumentsList[0] == InputKeys[9] || argumentsList[0] == InputKeys[10])
+                                {
+                                    if (!InputKeysCheck["animation"])
+                                    {
+                                        AnimationFile = argumentsList[1];
+
+                                        InputKeysCheck["animation"] = true;
+
+                                        argumentsList.Remove(argumentsList[1]);
+                                        argumentsList.Remove(argumentsList[0]);
+                                        
+                                        Console.WriteLine($"Path to animation file = {AnimationFile}");
+                                    }
+                                    else
+                                    {
+                                        throw new WrongCommandLineArgumentsException("Path to animation file should be set only once");
+                                    }
+                                }
+                                else if (argumentsList[0] == InputKeys[13] || argumentsList[0] == InputKeys[14])
+                                {
+                                    if (!InputKeysCheck["output"])
+                                    {
+                                        VideoFile = argumentsList[1];
+
+                                        InputKeysCheck["output"] = true;
+
+                                        argumentsList.Remove(argumentsList[1]);
+                                        argumentsList.Remove(argumentsList[0]);
+                                        
+                                        Console.WriteLine($"Path to output file = {VideoFile}");
+                                    }
+                                    else
+                                    {
+                                        throw new WrongCommandLineArgumentsException("Path to output file should be set only once");
+                                    }
+                                }
+                                else if (argumentsList[0] == InputKeys[11] || argumentsList[0] == InputKeys[12])
+                                {
+                                    if (!InputKeysCheck["models"])
+                                    {
+                                        bool exit = false;
+                                        
+                                        int i = 0;
+                                        foreach (string model in argumentsList)
+                                        {
+                                            for (int j = 0; j < InputKeys.Length;j++)
+                                            {
+                                                if (i != 0 && model == InputKeys[j])
+                                                {
+                                                    exit = true;
+                                                    break;
+                                                }
+                                            }
+
+                                            if (exit)
+                                            {
+                                                break;
+                                            }
+
+                                            if (i != 0)
+                                            {
+                                                ModelFiles.Add(model);
+                                                Console.WriteLine($"Model = {ModelFiles[ModelFiles.Count-1]}");
+                                                InputKeysCheck["models"] = true;
+                                            }
+
+                                            i++;
+                                        }
+
+                                        for (int k = i - 1; k >= 0; k--)
+                                        {
+                                            argumentsList.Remove(argumentsList[k]);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        throw new WrongCommandLineArgumentsException("Path (or paths) to model (or models) should be set only once");
+                                    }
+                                }
+                        }
                 }
 
-                if (AnimationFile.Count < MinimumAmountOfArguments)
+                if (ModelFiles.Count < 1 || AnimationFile == "" || VideoFile == "")
                 {
-                    throw new WrongCommandLineArgumentsException("The first command line arguments should be: 1. Path (or paths) for input file (or files) 2. Path for output file");
+                    throw new WrongCommandLineArgumentsException($"All {MinimumAmountOfArguments} required arguments should be used: path to animation file (-a), path to model (or models) (-m) and path to output video (-o)");
                 }
                 
-                VideoFile = AnimationFile[AnimationFile.Count - 1];
-                AnimationFile.Remove(AnimationFile[AnimationFile.Count - 1]);
+                VideoFile = ModelFiles[ModelFiles.Count - 1];
+                ModelFiles.Remove(ModelFiles[ModelFiles.Count - 1]);
                 
-                System.Console.Out.WriteLine($"Video file = {VideoFile}");
+                //System.Console.Out.WriteLine($"Video file = {VideoFile}");
                 
-                foreach (var animation in AnimationFile)
-                {
-                    System.Console.Out.WriteLine($"Animation file = {animation}");
-                }
+                //foreach (var animation in ModelFiles)
+                //{
+                    //System.Console.Out.WriteLine($"Animation file = {animation}");
+                //}
             }
         }
     }
