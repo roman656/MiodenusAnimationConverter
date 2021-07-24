fce5c697fc63421402fd093f7e91d8ed68941e38
roman656 <romanstrah@mail.ru>
2021-07-24 15:29:17 +0300

    Added more debug assets.

--- MiodenusAnimationConverter/MainController.cs
+++ MiodenusAnimationConverter/MainController.cs
@@ -19,8 +19,11 @@ namespace MiodenusAnimationConverter
         private readonly bool _isMainWindowVisible = true;
         private readonly byte _mainWindowFrequency = 60;
         private readonly string _animationFilename = "";
-        private readonly string[] _modelFilenames = { "DebugAssets/Rhm._Borsig_12.8.stl",
-                                                      "DebugAssets/IS-6.stl" };    // Временное решение.
+        private readonly string[] _modelFilenames = { "DebugAssets/Rhm_Borsig_12_8.stl",
+                                                      "DebugAssets/Jagdtiger.stl",
+                                                      "DebugAssets/IS-6.stl",
+                                                      "DebugAssets/Sphere.stl",
+                                                      "DebugAssets/Bottle.stl" };    // Временное решение.
         private Scene.Scene _scene = new ();
         
         public MainController(string[] args)
